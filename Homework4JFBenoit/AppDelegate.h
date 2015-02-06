51fe48cf07e0a9fa9a90613d1ea07f0644b1a553
jacob <github@benoits.com>
2015-02-05 20:13:13 -0800

    Initial Commit

---
+++ Homework4JFBenoit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Homework4JFBenoit
+//
+//  Created by Jacob Benoit on 2/5/15.
+//  Copyright (c) 2015 Jacob Benoit. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Homework4JFBenoit/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Homework4JFBenoit
+//
+//  Created by Jacob Benoit on 2/5/15.
+//  Copyright (c) 2015 Jacob Benoit. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
